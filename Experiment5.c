38fc9d5142068cafd81b07b5cd6e89908dbba056
Guthula Raja Venkatesh <148532866+rajavenkatesh04@users.noreply.github.com>
2023-10-27 20:22:00 +0530

    Experiment5.c

---
+++ Experiment5.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define a structure for a node in the linked list
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed.\n");
+        exit(1);
+    }
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a node at the end of the list
+void insertEnd(struct Node** head, int value) {
+    struct Node* newNode = createNode(value);
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        struct Node* current = *head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        current->next = newNode;
+    }
+}
+
+// Function to print the linked list
+void printList(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* myList = NULL;
+
+    // Insert elements at the end
+    insertEnd(&myList, 10);
+    insertEnd(&myList, 20);
+    insertEnd(&myList, 30);
+
+    // Print the linked list
+    printf("Linked List: ");
+    printList(myList);
+
+    return 0;
+}
