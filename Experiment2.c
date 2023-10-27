36be651f6bb3010b3caeb5d6bb2b2ca35518834f
Guthula Raja Venkatesh <148532866+rajavenkatesh04@users.noreply.github.com>
2023-10-27 20:00:37 +0530

    Experiment2.c

---
+++ Experiment2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+//Declaring Structure
+struct person
+{
+   int age;
+   float weight;
+};
+
+int main()
+{
+    //Declaring structure variable
+    struct person *personPtr, person1;
+    personPtr = &person1;   
+
+    printf("Enter age: ");
+    scanf("%d", &personPtr->age);
+
+    printf("Enter weight: ");
+    scanf("%f", &personPtr->weight);
+
+    printf("Displaying:\n");
+    printf("Age: %d\n", personPtr->age);
+    printf("weight: %f", personPtr->weight);
+
+    return 0;
+}
